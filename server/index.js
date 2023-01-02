838605448d7b16883a15f095cf897c720c74705b
intern_earn <earnearn.anchira@gmail.com>
2023-01-02 22:00:10 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+const { userInfo } = require('os');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "kiddraw"
+
+})

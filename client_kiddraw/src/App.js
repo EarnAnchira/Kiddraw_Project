1df55b66cea59e291ea922a2e7bf9f9ab61b510b
chatchanok <chatchanok.v@mail.kmutt.ac.th>
2023-01-07 18:52:18 +0700

    [refactor] set environment for front end dev

--- client_kiddraw/src/App.js
+++ client_kiddraw/src/App.js
@@ -1,7 +1,10 @@
+import './App.css';
+import Navbar from './components/navbar/navbar';
+
 function App() {
   return (
-    <div className="App">
-  
+    <div className="kidsdraw_app">
+      <Navbar/>
     </div>
   );
 }

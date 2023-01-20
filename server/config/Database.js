4952478e626b64d9112d4f9cd7614fad7301f9ce
intern_earn <earnearn.anchira@gmail.com>
2023-01-20 13:55:20 +0700

    Revert "no message"
    This reverts commit 72f79ff680a0101613d722da57242092928b853b.

--- server/config/Database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db=new Sequelize('kiddraw','root','root',{
-    host: 'localhost',
-    dialect: 'mysql',
-    port: '3000'
-});
-
-export default db;
